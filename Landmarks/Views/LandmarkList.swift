0bf7bbd72d04e22d18a7f8168ffffd139e549376
supreeth avadhani <supreethavadhani@gmail.com>
2022-03-26 10:08:56 +0530

    completed fav filter functionality

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
-        NavigationView {
-            List(landmarks){ landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                LandmarkRow(landmark: landmark)
-                }
-            }
-            .navigationTitle("Landmarks")
+    @State private var showFavoritesOnly = true
+    @EnvironmentObject var modelData: ModelData
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-}
+    
+    var body: some View {
+            NavigationView {
+                List {
+                    Toggle(isOn: $showFavoritesOnly){ Text("Favourites Only")
+                    }
+                    ForEach(filteredLandmarks){ landmark in
+                        NavigationLink{
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
+    }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
